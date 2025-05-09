9c1c2ef118a80483abfef451955b2c04576ead46
Basile <basileroth.75@gmail.com>
2025-05-09 18:40:58 +0200

    add: new prompt

--- offseason_greenlobby/modeling/open_ai.py
+++ offseason_greenlobby/modeling/open_ai.py
@@ -14,12 +14,14 @@ class OpenAIModel:
             raise ValueError("OPENAI_API_KEY environment variable is not set.")
         self.model_name = model_name
 
+    """
     @retry(
         stop=stop_after_attempt(3),
         wait=wait_exponential(multiplier=1, min=1, max=10),
         retry=retry_if_exception_type(Exception),
         reraise=True
     )
+    """
     def classify(self, exposes_sommaires, instruct_system, temperature) -> str:
         # Instancie un client OpenAI
         try:
@@ -30,14 +32,12 @@ class OpenAIModel:
                         "role": "user",
                         "content": f"Amendement {idx+1} : {message}"
                     })
-
                 response = client.chat.completions.create(
                     model=self.model_name,
                     n = len(messages)-1,
                     temperature= temperature,
                     messages = messages 
                 )
-                
                 return [choice.message.content.strip().lower() for choice in response.choices]
         except Exception as e:
             print(f"❌ API Error : {e}")
@@ -49,6 +49,7 @@ class OpenAIModel:
         for start in range(0, len(df), batch_size):
             end = start + batch_size
             batch = df.iloc[start:end]
+
             try:
                 content = self.classify(
                     exposes_sommaires = [row[COL_EXPOSE_SOMMAIRE] for _, row in batch.iterrows()],
@@ -57,7 +58,6 @@ class OpenAIModel:
                         DESCRIPTION=batch[COL_DESCRIPTION].values[0]
                     ),temperature=temperature
                 )
-
                 if batch.shape[0] == len(content):
                     print(f"✅ {len(content)} results found out of {batch.shape[0]}")
                     content_ret.extend(content)
